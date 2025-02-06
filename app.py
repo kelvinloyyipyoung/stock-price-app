9cb23c49e84d7f145093194dee39159629f1e5b7
Kelvin Young <kelvinloyyipyoung@gmail.com>
2025-02-06 15:08:14 +1100

    Implement colour visualisation of price change

--- app.py
+++ app.py
@@ -7,15 +7,19 @@ app = Flask(__name__)
 
 def get_stock_price(ticker):
     stock = yf.Ticker(ticker)
-    price = stock.history(period='1d')["Close"].iloc[-1]
-    return round(price, 2)
+    price = stock.history(period='2d')["Close"]
+    current_price = price.iloc[-1]
+    previous_price = price.iloc[-2]
+    change = "up" if current_price > previous_price else "down"
+    return round(current_price, 2), change
+
+# Home page
 
 @app.route("/")
 def index():
-    vas_price = get_stock_price("VAS.AX")
-    vgs_price = get_stock_price("VGS.AX")
-    return render_template("index.html", vas_price=vas_price, vgs_price=vgs_price)
+    vas_price, vas_change = get_stock_price("VAS.AX")
+    vgs_price, vgs_change = get_stock_price("VGS.AX")
+    return render_template("index.html", vas_price=vas_price, vas_change=vas_change, vgs_price=vgs_price, vgs_change=vgs_change)
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
